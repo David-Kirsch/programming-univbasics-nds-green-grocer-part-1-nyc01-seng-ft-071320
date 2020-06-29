40e530e529e8881a27f8f8cf53380601e16ae6f7
David Kirsch <davidjkirsch@gmail.com>
2020-06-29 17:37:34 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -22,7 +22,10 @@ def consolidate_cart(cart)
   cart.each do |item|
     if(!find_item_by_name_in_collection(item[:item], receipt))
       receipt.push(item)
+      receipt[item][:count] = 1 
       binding.pry
+    else
+      receipt[item][:count]+=1 
        
     end
   end
