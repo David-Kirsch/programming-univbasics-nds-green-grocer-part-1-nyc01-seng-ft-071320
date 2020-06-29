df137ab4d3ac281e66e3f8200a5434b1f34d7546
David Kirsch <davidjkirsch@gmail.com>
2020-06-29 16:32:02 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,11 +19,9 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   receipt = []
-  while(x < cart.length) do
-    if(receipt[cart][:item] == nil)
-
-      binding.pry
-    end
+  cart.each_with_object({}) do |(key, value), final|
+    binding.pry
+  end
   
 end
 
