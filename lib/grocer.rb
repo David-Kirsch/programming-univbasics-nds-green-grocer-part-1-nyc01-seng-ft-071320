cf5c130bddd1a13d5f32862c6b30668f9700e644
David Kirsch <davidjkirsch@gmail.com>
2020-06-29 17:33:39 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -20,8 +20,9 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   receipt = []
   cart.each do |item|
+    #if(!find_item_by_name_in_collection(item[:item], receipt))
     item.each_with_object({}) do |(key, value), itemDetail|
-       if(!find_item_by_name_in_collection(value, receipt))
+       if(!find_item_by_name_in_collection(value[:item], receipt))
          receipt.push(item)
          binding.pry
        end
