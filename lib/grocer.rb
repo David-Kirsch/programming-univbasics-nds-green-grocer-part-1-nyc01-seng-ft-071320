14a345f1abf60ee6fce9dd7c5d5571c3cd4a9db4
David Kirsch <davidjkirsch@gmail.com>
2020-06-29 16:00:32 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -4,7 +4,8 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-   collection.each do |item, value|
+   collection.each do |item|
+     
      binding.pry
   end
 
