6bc62021c5afc2b5e5cde07f90979d2cb8b52870
David Kirsch <davidjkirsch@gmail.com>
2020-06-29 16:03:07 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,8 +5,9 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
    collection.each do |item|
-     
-     binding.pry
+     if(item[:item] == name)
+       item
+     end
   end
 
 end
